5329de6929ac0186904b9b5d4db739afa5954376
MUHIBOLLAH <muhebollah.diu@gmail.com>
2021-07-11 15:06:17 +0600

    config.EnableCors

--- Attendance_API/App_Start/WebApiConfig.cs
+++ Attendance_API/App_Start/WebApiConfig.cs
@@ -13,6 +13,8 @@ namespace Attendance_API
     {
         public static void Register(HttpConfiguration config)
         {
+            config.EnableCors();
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
