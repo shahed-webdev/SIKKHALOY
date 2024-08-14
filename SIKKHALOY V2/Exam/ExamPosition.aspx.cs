f25f12b5f9f6e4972487ec117164ab9d3c13cf02
MUHIBULLAH <muhebollah.diu@gmail.com>
2024-08-14 21:51:24 +0600

    add cell index in another loop

--- SIKKHALOY V2/Exam/ExamPosition.aspx.cs
+++ SIKKHALOY V2/Exam/ExamPosition.aspx.cs
@@ -238,10 +238,12 @@ namespace EDUCATION.COM.Exam
                         Text += ", Grade: " + StudentsGridView.DataKeys[row.RowIndex]["Student_Grade"];
                         Text += ", Point: " + Convert.ToDecimal(StudentsGridView.DataKeys[row.RowIndex]["Student_Point"].ToString()).ToString("0.00");
                         Text += ", Position In Class: " + row.Cells[9].Text;
+                      
                         if (SecPositionCheckBox.Checked)
                         {
                             Text += ", Position In Section: " + row.Cells[10].Text;
                         }
+
                         Text += ". Regards: " + Session["School_Name"];
                     }
                     else
@@ -282,11 +284,13 @@ namespace EDUCATION.COM.Exam
                                 Text += "Total Marks: " + Convert.ToDecimal(StudentsGridView.DataKeys[row.RowIndex]["ObtainedMark_ofStudent"].ToString()).ToString("0.00");
                                 Text += ", Grade: " + StudentsGridView.DataKeys[row.RowIndex]["Student_Grade"];
                                 Text += ", Point: " + Convert.ToDecimal(StudentsGridView.DataKeys[row.RowIndex]["Student_Point"].ToString()).ToString("0.00");
-                                Text += ", Position In Class: " + row.Cells[8].Text;
+                                Text += ", Position In Class: " + row.Cells[9].Text;
+                              
                                 if (SecPositionCheckBox.Checked)
                                 {
-                                    Text += ", Position In Section: " + row.Cells[9].Text;
+                                    Text += ", Position In Section: " + row.Cells[10].Text;
                                 }
+
                                 Text += ". Regards: " + Session["School_Name"];
                             }
                             else
